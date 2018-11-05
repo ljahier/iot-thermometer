b8912670a5d98fc3bcb487766a46fd4b5c09f607
Lucas JAHIER <lucas@lucasjahier.fr>
2018-11-05 16:46:10 +0100

    lol

--- index.js
+++ index.js
@@ -10,19 +10,23 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.get('/', (req, res) => {
-
+    res.render('index', {
+        username: "Lucas"
+    })
 })
 
 app.get('/post/:data', (req, res) => {
     fs.writeFile('message.txt', req.params.data, (err) => {
-        console.log('The file has been saved!');
-    });
-    res.end(req.params.data)
-})
-
-app.get('/post/:data', (req, res) => {
-    fs.writeFile('message.txt', req.params.data, (err) => {
+        fs.readFile('message.txt', (err, data) => {
+            if (err) throw err;
+            res.render('index', {
+                data: data
+            })
+        })
         console.log('The file has been saved!');
     });
     res.end(req.params.data)
