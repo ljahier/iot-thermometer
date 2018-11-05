c4a5d287310112c6a209d827d2613ff53c507049
Lucas JAHIER <lucas@lucasjahier.fr>
2018-11-05 16:12:19 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser')
+const ejs = require('ejs');
+
+let app = express();
+let port = process.env.PORT | 8080;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.get('/', (req, res) => {
+    fs.readFile('./index.html', (err, data) => {
+        res.end(data);
+    });
+
+})
+
+app.get('/post/:data', (req, res) => {
+    fs.writeFile('message.txt', req.params.data, (err) => {
+        console.log('The file has been saved!');
+    });
+    res.end(req.params.data)
+})
+
+app.post('/post/:data', (req, res) => {
+    fs.writeFile('message.txt', req.params.data, (err) => {
+        console.log('The file has been saved!');
+    });
+    res.end(req.params.data)
+})
+
+app.listen(port, () => {
+    console.log(`Server running on localhost:${port}`);
+});
